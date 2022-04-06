24e7c106d94b0f291fdb78b36a4580d1e86a4c71
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-04-06 18:45:00 +0200

    eksperimenterer med radioknapperner og deres funktion

--- src/main/java/dat/startcode/control/ChooseCupcakeServlet.java
+++ src/main/java/dat/startcode/control/ChooseCupcakeServlet.java
@@ -1,10 +1,14 @@
 package dat.startcode.control;
 
+import dat.startcode.model.entities.ChosenCupcake;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ChooseCupcakeServlet", value = "/ChooseCupcakeServlet")
 public class ChooseCupcakeServlet extends HttpServlet {
@@ -15,11 +19,15 @@ public class ChooseCupcakeServlet extends HttpServlet {
         String selectedRadiobuttonValueButtom = request.getParameter("flavorpricebuttom");
         String selectedRadiobuttonValueTopping = request.getParameter("flavorpricetopping");
 
+        ChosenCupcake chosenCupcake = new ChosenCupcake(selectedRadiobuttonValueButtom, selectedRadiobuttonValueTopping, 5, 5);
+        //TODO: skal hente priserne fra databasen
 
+        List<ChosenCupcake> chosenCupcakeList = new ArrayList<>();
+        chosenCupcakeList.add(chosenCupcake);
 
+        request.setAttribute("chosencupcakelist", chosenCupcakeList);
 
-
-        request.getRequestDispatcher("WEB-INF/tags/opretbruger.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/chosencupcaketemp.jsp").forward(request, response);
     }
 
     @Override
--- src/main/java/dat/startcode/model/entities/ChosenCupcake.java
+++ src/main/java/dat/startcode/model/entities/ChosenCupcake.java
@@ -1,4 +1,67 @@
 package dat.startcode.model.entities;
 
 public class ChosenCupcake {
+    private String buttom;
+    private String topping;
+    private int cupcakePrice;
+    private int buttomPrice;
+    private int toppingPrice;
+
+
+    public ChosenCupcake(String buttom, String topping, int buttomPrice, int toppingPrice) {
+        this.buttom = buttom;
+        this.topping = topping;
+        this.cupcakePrice = buttomPrice + toppingPrice;
+    }
+
+    public String getButtom() {
+        return buttom;
+    }
+
+    public void setButtom(String buttom) {
+        this.buttom = buttom;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    public int getCupcakePrice() {
+        return cupcakePrice;
+    }
+
+    public void setCupcakePrice(int cupcakePrice) {
+        this.cupcakePrice = cupcakePrice;
+    }
+
+    public int getButtomPrice() {
+        return buttomPrice;
+    }
+
+    public void setButtomPrice(int buttomPrice) {
+        this.buttomPrice = buttomPrice;
+    }
+
+    public int getToppingPrice() {
+        return toppingPrice;
+    }
+
+    public void setToppingPrice(int toppingPrice) {
+        this.toppingPrice = toppingPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ChosenCupcake{" +
+                "buttom='" + buttom + '\'' +
+                ", topping='" + topping + '\'' +
+                ", cupcakePrice=" + cupcakePrice +
+                ", buttomPrice=" + buttomPrice +
+                ", toppingPrice=" + toppingPrice +
+                '}';
+    }
 }
